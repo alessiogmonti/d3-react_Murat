553edd04ba0c232748e8e430be9ffe5fdcd9bb1e
sergio monti <sergiomonti95@sergios-MacBook-Pro.local>
2022-07-03 18:30:34 +0200

    v1_02

--- app-01/src/App.js
+++ app-01/src/App.js
@@ -1,34 +1,32 @@
-import React, {useRef, useEffect} from 'react'
+import React, {useRef, useEffect, useState} from 'react'
 import {select } from 'd3';
 
-const data = [ 20, 45, 55, 35, 66];
+const init = [20, 45, 55, 35, 66];
 
 function App() {
   const svgRef = useRef();
+  const [data, setData] = useState(init)
   
   useEffect(() => {
     const svg = select(svgRef.current)
-    svg.selectAll("circle")
+    svg
+      .selectAll("circle")
       .data(data)
-      .join(
-        enter => enter
-          .append("circle")
-          .attr("r", value=> value / 2 )
-          .attr("cx", value=> value * 2)
-          .attr("cy", value => value * 2)
-          .attr("stroke", "red"),
-
-        update => update.attr("class", "updated"),
-        exit => exit.remove()
-      )
-  }, [])
+      .join("circle")
+      .attr("r", value=> value )
+      .attr("cx", value=> value * 2)
+      .attr("cy", value => value * 2)
+      .attr("stroke", "red");
+  }, [data])
   return (
     <div className="App">
       <header className="App-header">
         <h1> learning d3 + react!</h1>
       </header>
-      <svg ref={svgRef}>
-      </svg>
+      <svg ref={svgRef}></svg>
+      <button onClick={() => setData(data.map(value => value +5))}>Update Data</button>
+      <button onClick={() => setData(data.filter(value => value <= 35))}>Filter Data</button>
+      <button onClick={() => setData(init)}>Reset Data</button>
     </div>
   );
 }
